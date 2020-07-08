5001100069e1dfa9993ae41c4a75166f5c73d729
naslakboss <damiannaglak1@gmail.com>
2020-07-08 11:45:53 +0200

    first commit

---
+++ podlasietrain/src/test/java/naslakcode/podlasietrain/PodlasietrainApplicationTests.java
@@ -0,0 +1,13 @@
+package naslakcode.podlasietrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodlasietrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
